cb23655ad5e1174b4716bfe0a8ead36869a84454
Alok Kuchlous <alokk@Aloks-MacBook-Pro.local>
2015-01-17 22:47:00 +0530

    Replace db with just javascript data-struct
    We do not really need db for categories because the data is read-only
    so we can just pre-populate javascript data structures.

--- app/alloy.js
+++ app/alloy.js
@@ -10,6 +10,7 @@
 //
 // Alloy.Globals.someGlobalFunction = function(){};
 
-Alloy.Collections.categories = Alloy.createCollection('category');
-Alloy.Collections.sites = Alloy.createCollection('site');
-Alloy.Collections.gtypes = Alloy.createCollection('gtype');
+var data = require ('data');
+
+Alloy.Globals.categories = data.data.categories;
+Alloy.Globals.sites = data.data.sites;
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,47 +1,10 @@
-var data = require ('data');
-
-
-var createDb = function(data) {
-	_.each(data.data.types, function(t) {
-		var gtype = Alloy.createModel('gtype', t);
-		Alloy.Collections.gtypes.add(gtype);
-	});
-
-	_.each(data.data.categories, function(d) {
-		var category = Alloy.createModel('category', d);
-		Alloy.Collections.categories.add(category);
-	});
-
-	_.each(data.data.sites, function(s) {
-		var site = Alloy.createModel('site', s);
-		Alloy.Collections.sites.add(site);
-	});
-	
-	console.log("Categories from db");
-	Alloy.Collections.categories.each(function(category) {
- 		console.log('category: ' + category.get('title') + ' ' + category.get('type') + ' ' + category.get('id'));
-	});
-
-	console.log("Sites from db");
-	Alloy.Collections.sites.each(function (t) {
-		console.log(t.get('id') + ' ' + t.get('name'));
-	});
-	
-	console.log("Types from db");
-	Alloy.Collections.gtypes.each(function (t) {
-		console.log(t.get('id') + ' ' + t.get('site_id') + ' ' + t.get('title'));
-	});
-};
-
 function doClick(e) {
-    console.log('In doClick');
-    // var category = categories.get(24);
-    // alert(category.get('title'));
+	console.log('In doClick');
 	
-	var type_id = Math.round(Math.random() * 15 + 1);
-	var t = Alloy.Collections.gtypes.where({'site_id': 1})[0];
-	alert('Aadi has ' + t.get('title') + ' and lives in ' + Alloy.Collections.sites.get(t.get('site_id')).get('name'));
+	var type_id = Math.round(Math.random() * 100 + 1);
+	console.log("type_id: " + type_id);
+	
+	alert('Aadi has ' + Alloy.Globals.categories[type_id].title);
 }
 
-createDb(data);
 $.index.open();
