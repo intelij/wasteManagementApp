aa93e78e53815812fc5a9db96d351d906ba0e0cb
sandeep goud <sandeepgp@mirafra.com>
2015-01-21 15:30:10 +0530

    added description.

--- app/controllers/categories.js
+++ app/controllers/categories.js
@@ -1,5 +1,5 @@
-var args = arguments[0] || {};
+var params = arguments[0] || {};
 
-$.topbar.setTitle(args.data.description.word);
-$.categoryLabel.text = 'Category Type: ' + args.data.description.categories[0].type;
-$.descriptionLabel.text = 'Description:';
+$.topbar.setTitle(params.data.title);
+$.categoryLabel.text = 'Category Type: ' + params.data.description.type;
+$.descriptionLabel.text = 'Description: You need to dump this material in ' + params.type + " as per your complex guidelines";
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,62 +2,65 @@ var autocomplete = require('autocomplete');
 var synonyms = require('data').data.synonyms;
 
 var data = Alloy.Globals.categories;
-var complexId = 1;
-var complexDetail = _.findWhere(Alloy.Globals.sites, {
-	"id" : complexId,
-});
 
 var auto = new autocomplete.autocomplete(data, synonyms);
-function showSuggestions(data) {
-	var tableData = [];
-	_.each(data, function(element, index, list) {
-		var resultRow = Ti.UI.createTableViewRow({
-			title : element.title,
-			hasChild : true,
-			color : "black",
-			description : element,
-			backgroundColor : "white",
-			height : 60,
-			top : 1
-		});
-		tableData.push(resultRow);
-	});
 
-	if (tableData.length > 0) {
-		$.resultsTable.data = tableData;
-		var rowCount = $.resultsTable.getSections()[0].rowCount;
-		var rowHeight = $.resultsTable.rowHeight;
-		$.resultsTable.height = (rowCount * rowHeight) + 50;
-	} else {
-		$.resultsTable.height = 50;
-	}
+ var complexId = 1;
+  var complexDetail = Alloy.Globals.sites[complexId];
+ 
+function showSuggestions(data) {
+  var tableData = [];
+  _.each(data, function(element, index, list) {
+    var resultRow = Ti.UI.createTableViewRow({
+      title : element.title,
+      hasChild : true,
+      color : "black",
+      description : element,
+      backgroundColor : "white",
+      height : 60,
+      top : 1
+    });
+    tableData.push(resultRow);
+  });
+
+  if (tableData.length > 0) {
+    $.resultsTable.data = tableData;
+    var rowCount = $.resultsTable.getSections()[0].rowCount;
+    var rowHeight = $.resultsTable.rowHeight;
+    $.resultsTable.height = (rowCount * rowHeight) + 50;
+  } else {
+    $.resultsTable.height = 50;
+  }
 }
 
 function getCategoryId(title) {
-	var category;
-	Object.keys(data).forEach(function(k) {
-		if (data[k].title == title) {
-			category = k;
-		}
-	});
-	return category;
+  var category;
+  Object.keys(data).forEach(function(k) {
+    if (data[k].title == title) {
+      category = k;
+    }
+  });
+  return category;
 }
 
-function selectWord(e) {
-	getCategoryId(e.rowData.title);
-	alert(result);
-	Alloy.createController('/categories', {
-		data : e.rowData
-	}).getView().open();
+function selectWord (e) {
+ 
+  var categoryID = getCategoryId(e.rowData.title);
+  var typeId = complexDetail.classification[categoryID];
+  var typeDesc = complexDetail.types[typeId].title;
+  Alloy.createController('/categories', {
+    data : e.rowData,
+    type: typeDesc
+  }).getView().open();
 }
 
 function searchWord(e) {
-	if (e.source.value) {
-		var searchResult = auto.search(e.source.value);
-		showSuggestions(searchResult);
-	} else {
-		$.resultsTable.height = 50;
-	}
+  if (e.source.value) {
+    var searchResult = auto.search(e.source.value);
+    showSuggestions(searchResult);
+  } else {
+    $.resultsTable.height = 50;
+  }
 }
 
 $.topbar.setTitle("Waste Segregation");
