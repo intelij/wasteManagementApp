d80b2f3151408b50f4622c007313d1f6ab2a9b88
ykamara <Mirafra_Mac1@Mirafras-Mac-mini.local>
2015-01-21 14:27:42 +0530

    formatted code

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,29 +1,29 @@
 var autocomplete = require('autocomplete');
 var synonyms = require('data').data.synonyms;
 
-var data=Alloy.Globals.categories;
-var complexId=1;
-var complexDetail=_.findWhere(Alloy.Globals.sites, {
-		"id":complexId,
-	}); 
+var data = Alloy.Globals.categories;
+var complexId = 1;
+var complexDetail = _.findWhere(Alloy.Globals.sites, {
+	"id" : complexId,
+});
 
 var auto = new autocomplete.autocomplete(data, synonyms);
-function showSuggestions(data){
+function showSuggestions(data) {
 	var tableData = [];
 	_.each(data, function(element, index, list) {
-		var resultRow =  Ti.UI.createTableViewRow({
+		var resultRow = Ti.UI.createTableViewRow({
 			title : element.title,
 			hasChild : true,
-			color: "black",
-			description:element,
-			backgroundColor:"white",
-			height:60,
-			top:1
+			color : "black",
+			description : element,
+			backgroundColor : "white",
+			height : 60,
+			top : 1
 		});
 		tableData.push(resultRow);
 	});
-	
-	if(tableData.length > 0) {	
+
+	if (tableData.length > 0) {
 		$.resultsTable.data = tableData;
 		var rowCount = $.resultsTable.getSections()[0].rowCount;
 		var rowHeight = $.resultsTable.rowHeight;
@@ -33,30 +33,32 @@ function showSuggestions(data){
 	}
 }
 
-function getCategoryId (title){
-	var category ;
-	Object.keys(data).forEach(function(k){
-		if(data[k].title == title){
-			category =  k;
+function getCategoryId(title) {
+	var category;
+	Object.keys(data).forEach(function(k) {
+		if (data[k].title == title) {
+			category = k;
 		}
 	});
 	return category;
 }
 
-function selectWord(e){
+function selectWord(e) {
 	getCategoryId(e.rowData.title);
 	alert(result);
-	Alloy.createController('/categories', {data:e.rowData}).getView().open();
+	Alloy.createController('/categories', {
+		data : e.rowData
+	}).getView().open();
 }
 
 function searchWord(e) {
-	if(e.source.value){
+	if (e.source.value) {
 		var searchResult = auto.search(e.source.value);
-    	showSuggestions(searchResult);
-	}else{
+		showSuggestions(searchResult);
+	} else {
 		$.resultsTable.height = 50;
 	}
- }  
+}
 
 $.topbar.setTitle("Waste Segregation");
 $.index.open();
