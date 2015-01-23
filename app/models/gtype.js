f50e94404f9fe797ab5f8b0269ac31f3cc5a4077
sandeep goud <sandeepgp@mirafra.com>
2015-01-23 17:45:21 +0530

    used App Properties and fixed the issue in first screen.

--- app/models/gtype.js
+++ app/models/gtype.js
@@ -1,19 +1,19 @@
 exports.definition = {
-  config: {
-    'columns': {
-      'site_id': 'integer',
-      'title': 'Text',
-      'description': 'Text'
+  config : {
+    'columns' : {
+      'site_id' : 'integer',
+      'title' : 'Text',
+      'description' : 'Text'
     },
-    'defaults': {
-      'site_id': 0,
-      'title': '-',
-      'description': '-'
+    'defaults' : {
+      'site_id' : 0,
+      'title' : '-',
+      'description' : '-'
     },
-    'adapter': {
-      'type': 'sql',
-      'collection_name': 'gtypes',
-      'idAttribute': 'id'
+    'adapter' : {
+      'type' : 'sql',
+      'collection_name' : 'gtypes',
+      'idAttribute' : 'id'
     }
   }
 };
